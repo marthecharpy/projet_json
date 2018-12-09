20d0ef1eda9a787992ee64ff2c45547a3c3b93bb
marthe.charpy <marthe.charpy@gmail.com>
2018-12-09 23:24:28 +0100

    creation db.json

--- src/com/monprojetJson/posts/PostManager.java
+++ src/com/monprojetJson/posts/PostManager.java
@@ -1,9 +1,99 @@
 package com.monprojetJson.posts;
 
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 
 public class PostManager {
 	
 	private ArrayList<Post> posts;
+	URL url = null;
+	try {
+		url = new URL("https://my-json-server.typicode.com/wasthishelpful/my-java-blog/posts");
+	} catch (MalformedURLException e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
+	}
+
+	HttpURLConnection connection = null;
+	int status = 0;
+	try {
+		connection = (HttpURLConnection) url.openConnection();
+		connection.setRequestMethod("GET");
+		status = connection.getResponseCode();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+
+	if(status == 200) {
+		InputStreamReader in = null;
+		try {
+			in = new InputStreamReader(
+				connection.getInputStream()
+			);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+					
+		JSONParser parser = new JSONParser();
+        Object jsonParsed = null;
+        try{
+            jsonParsed = parser.parse(in);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+		
+		try {
+			in.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		connection.disconnect();
+		
+		JSONArray root = (JSONArray) jsonParsed;
+		for(int i=0; i<root.size(); i++){
+			JSONObject postObject = (JSONObject) root.get(i);
+			int id = Math.toIntExact((long) postObject.get("id"));
+			String title = (String) postObject.get("title");
+			String content = (String) postObject.get("content");
+	
+			this.posts.add(new Post(id, title, content));
+			}
+	}
+	
+
+	public PostManager() {
+		this.posts= new ArrayList<Post>();
+		posts.add(new Post(1, "Lorem Elsass", "Lorem Elsass ipsum leo libero. jetz gehts los picon bière quam, consectetur dignissim sit geïz gravida sed Huguette sit Coopé de Truchtersheim pellentesque vulputate tristique id"));
+		posts.add(new Post (1, "Lorem ipsum","Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim."));
+		
+	}
+	
+	public ArrayList<Post>getAllPosts(){
+		return this.posts;		
+	}
+	
+	public Post getPostById(int id) {
+		for(var post : this.posts) {
+			if(post.getId() == id) {
+				return post;
+			}
+		}
+		
+		return null;
+}
 
 }
--- src/com/monprojetJson/postsServlet.java
+++ src/com/monprojetJson/postsServlet.java
@@ -1,7 +1,7 @@
 package com.monprojetJson;
 
 import java.io.IOException;
-import java.util.ArrayList;
+
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.monprojetJson.posts.Post;
+import com.monprojetJson.posts.PostManager;
 
 /**
  * Servlet implementation class postsServlet
@@ -17,23 +18,22 @@ import com.monprojetJson.posts.Post;
 @WebServlet("/posts")
 public class postsServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+    
+	private PostManager postManager;
     /**
      * @see HttpServlet#HttpServlet()
      */
     public postsServlet() {
         super();
-        // TODO Auto-generated constructor stub
+        
+        this.postManager= new PostManager();
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		var posts= new ArrayList<Post>();
-		posts.add(new Post(1, "Lorem Elsass", "Lorem Elsass ipsum leo libero. jetz gehts los picon bière quam, consectetur dignissim sit geïz gravida sed Huguette sit Coopé de Truchtersheim pellentesque vulputate tristique id"));
-		posts.add(new Post (1, "Lorem ipsum","Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim."));
-		request.setAttribute("posts", posts);
+		request.setAttribute("posts", this.postManager.getAllPosts());
 		this.getServletContext().getRequestDispatcher("/WEB-INF/posts/index.jsp").forward(request, response);
 	}
 
